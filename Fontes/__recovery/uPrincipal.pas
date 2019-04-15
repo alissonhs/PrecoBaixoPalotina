73a4191659a125a637a211c7606319d3d017ede5
alissonhs <alisson_hs@hotmail.com>
2019-04-15 20:23:46 -0300

    Tela Inicial Finalizada

---
+++ Fontes/__recovery/uPrincipal.pas
@@ -0,0 +1,37 @@
+unit uPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.MultiView, FMX.StdCtrls, FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    ToolBar1: TToolBar;
+    Label1: TLabel;
+    MultiView1: TMultiView;
+    Image1: TImage;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
